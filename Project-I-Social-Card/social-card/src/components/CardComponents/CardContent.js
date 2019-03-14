76c338e6cfa1c15cdfc64520feca3ae00a787df5
EvaGurtovaia <eva.gurtovaya@gmail.com>
2019-03-13 20:18:55 -0500

    Social Card MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+    return <div className ="cardContent"> <span className ="cardHeader">Get started with React</span>
+        <span className="cardPar">React makes it painless to create iteractive Uls. 
+            Design simple views for each starte 
+        in your application.</span>
+        <span className="cardWeb">reactjs.org</span>
+    </div>
+  };
+
+  export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
-import './Header.css';
+import './HeaderTitle.css';
+
+
+const HeaderTitle = () => {
+    return <div className="headertitle"><span className= "header" >Lambda School</span>
+    <span className= "text">@LambdaSchool</span>
+    <span className= "text"> 26 Jan</span>
+    </div>
+  };
+
+  export default HeaderTitle;
+
